5a8666f89239699c19d06b8b494baf7b9f93bc86
qwyx <mc010116@gmail.com>
2025-05-13 15:51:07 +0200

    finished 13

--- ex13.js
+++ ex13.js
@@ -6,9 +6,7 @@ const livres = [
   ];
   
   function trierLivres(livres) {
-    for (let index = 0; index < livres.length; index++) {
-        
-    }
+    return livres.sort((a, b) => a.year > b.year ? 1:-1)
   }
   
   console.log(trierLivres(livres)); // Output : [{ title: 'Book B', year: 1987 }, { title: 'Book A', year: 1995 }, { title: 'Book D', year: 1999 }, { title: 'Book C', year: 2005 }]
